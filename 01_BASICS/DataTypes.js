2774ae641f2b7799a0ab7a0d0dd2a5fd528a200e
Debottam Talapatra <talapatradebottam2004@gmail.com>
2025-08-26 23:54:27 +0530

    DAY ONE

---
+++ 01_BASICS/DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //Treat all JS Code as newer version. But even if we dont write it, it is implemented anyways. But writing it explicitly forces us to stick to the new version of JS throughout our code
+
+let name = "Debottam";
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value!
+//undefined
+//symbols => unique
+
+//objects
+
+console.log(typeof "Debottam");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
